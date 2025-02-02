32183c2e7e4eeb321ac9b4189066f25c6b905717
NaOH_HaN <69073248+NaOH-HaN@users.noreply.github.com>
2025-02-02 12:02:30 +0800

    Apply the Apache-2.0 license.

--- src/main.py
+++ src/main.py
@@ -323,11 +323,11 @@ def handle_about():
     
     ChatWith Client
     - Version 0.23
-    - Copyright © 2025 NaOH_HaN Partial reservation of rights
+    - Copyright © 2025 NaOH_HaN 
 
     本软件使用 Apache License 2.0 开源协议
     Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
+    You may not use this file except in compliance with the License.
     You may obtain a copy of the License at [https://www.apache.org/licenses/LICENSE-2.0].
     """
     print(about_info)
